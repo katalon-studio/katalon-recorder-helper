01c3347c19952088f937b295b4a3fda823116914
minhthanh3145 <mitatoto1997@gmail.com>
2021-02-22 15:39:10 +0700

    Fixed unit test

--- src/test/java/com/katalon/katalonrecorder/helper/command/TestExecutionCommandController.java
+++ src/test/java/com/katalon/katalonrecorder/helper/command/TestExecutionCommandController.java
@@ -27,13 +27,16 @@ public class TestExecutionCommandController {
     @Test
     public void testExecuteShOnMac() throws IOException, InterruptedException {
         if(!System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-            String param = "a string";
+            String param1 = "first string";
+            String param2 = "second string";
+            String param3 = "third string";
             ClassLoader classLoader = getClass().getClassLoader();
             File shFile = new File(classLoader.getResource("echo_sh.sh").getFile());
             Runtime.getRuntime().exec("chmod u+x " + shFile.getAbsolutePath());
-            CommandResource cmdResource = controller.executeSh(shFile.getAbsolutePath(), new String[]{param});
-            Assert.assertEquals(1, cmdResource.getOutput().size());
-            Assert.assertEquals(param, cmdResource.getOutput().get(0));
+            CommandResource cmdResource = controller.executeSh(shFile.getAbsolutePath(), new String[]{param1, param2, param3});
+            Assert.assertEquals(3, cmdResource.getOutput().size());
+            Assert.assertEquals(String.join(" ", param1, param2, param3)
+                    , cmdResource.getOutput().get(0));
         }
     }
 }
